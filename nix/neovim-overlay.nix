40297922bd750efffa260f30ced7957e0a4b2282
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-11-29 17:26:49 -0500

    wip

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -28,6 +28,7 @@ with final.pkgs.lib; let
   all-plugins = with pkgs.vimPlugins; [
     # plugins from nixpkgs go in here.
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
+    clang-tools
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
     # nvim-cmp (autocompletion) and extensions
@@ -88,6 +89,7 @@ with final.pkgs.lib; let
     lua-language-server
     libclang
     clang-tools
+    clang
     rust-analyzer
     python3
     nodejs_22
